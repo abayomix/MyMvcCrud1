c176596d188a8f3d504175797cfba2a1ca43706c
A.Adejare <adejare.abayo@gmail.com>
2023-10-22 10:23:12 -0700

    Add project files.

---
+++ MyMvcCrud1/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MyMvcCrud1.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-TFQ9M1R;Database=EmployDB;Integrated Security=true;");
+                // need to add connection string here after that do migration.
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
